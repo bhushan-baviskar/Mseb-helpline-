baf5807b5f4c243a46e4680f4144fa5cbff6bfe2
Bhushan Baviskar <baviskarbhushan5@gmail.com>
2022-12-16 20:15:29 +0530

    Version 1.5 responsive site

---
+++ script.js
@@ -0,0 +1,8 @@
+const donation = document.querySelector(".donation");
+
+donation.classList.add("active");
+
+// window.addEventListener("scroll", function () {
+//   donation.classList.add("off");
+//   donation.classList.remove("active");
+// });
